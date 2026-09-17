636ffcf2ce78033c13eb8e7266e93656669e5642
Krishna Arjun Addanki <krishnaarjunaddanki@Arjuns-MacBook-Pro.local>
2026-09-17 10:59:26 +0530

    Configure GitHub Pages workflows

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/Run2Studio" : "",
+  assetPrefix: isProd ? "/Run2Studio/" : "",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
